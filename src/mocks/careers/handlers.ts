a81344b18c425498c234c5a38c60e2662d69bfda
JinMyeong Je <dev.jinni520@gmail.com>
2026-01-05 15:30:47 +0900

    add: mock data

--- src/mocks/careers/handlers.ts
+++ src/mocks/careers/handlers.ts
@@ -1,12 +1,9 @@
-import { http, HttpResponse } from 'msw';
-import { mockCareers } from './data';
+import { http, HttpResponse } from "msw";
+import { mockCareers } from "./data";
 
 export const careerHandlers = [
   // 프로젝트 목록 조회 (페이지네이션)
-  http.get('/api/careers', ({ request }) => {
-    const url = new URL(request.url);
-
-
+  http.get("/api/careers", () => {
     return HttpResponse.json({
       success: true,
       data: {
@@ -16,4 +13,3 @@ export const careerHandlers = [
     });
   }),
 ];
-
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,9 @@
-import { projectHandlers } from './projects';
-import { skillHandlers } from './skills';
+import { projectHandlers } from "./projects";
+import { skillHandlers } from "./skills";
+import { careerHandlers } from "./careers";
 
 export const handlers = [
   ...projectHandlers,
   ...skillHandlers,
+  ...careerHandlers,
 ];
