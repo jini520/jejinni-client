d507bb9558b943f4724aae42fd527c529f28b709
JinMyeong Je <dev.jinni520@gmail.com>
2026-02-23 16:05:44 +0900

    style: 화면 사이즈에 따라 출력되는 카드 개수 수정

--- src/app/(sections)/BlogPostSection/BlogCards/BlogCards.tsx
+++ src/app/(sections)/BlogPostSection/BlogCards/BlogCards.tsx
@@ -5,32 +5,40 @@ import { VelogPost } from "@/types/velog.types";
 import LiquidGlass from "@/app/_components/LiquidGlass/LiquidGlass";
 import BlogCard from "../BlogCard/BlogCard";
 import Link from "next/link";
+import { useMediaQueryContext } from "@/context/mediaQueryContext";
 import "./blog-cards.scss";
 
 interface BlogCardsProps {
   posts: VelogPost[];
 }
 
-const MIN_DISPLAY_COUNT = 3;
-const MAX_DISPLAY_COUNT = 9;
-
 const BlogCards = ({ posts }: BlogCardsProps) => {
-  const [displayCount, setDisplayCount] = useState(
-    Math.min(posts.length, MIN_DISPLAY_COUNT)
-  );
-  const [hasMore, setHasMore] = useState(posts.length > displayCount);
+  const { isMobile, isTablet, isDesktop, isWideDesktop } =
+    useMediaQueryContext();
+
+  const MIN_DISPLAY_COUNT = useMemo(() => {
+    if (isTablet) return 4;
+    if (isDesktop || isWideDesktop) return 6;
+    return 3;
+  }, [isMobile, isTablet, isDesktop, isWideDesktop]);
+
+  const MAX_DISPLAY_COUNT = useMemo(() => {
+    if (isTablet) return 8;
+    if (isDesktop || isWideDesktop) return 12;
+    return 3;
+  }, [isMobile, isTablet, isDesktop]);
+
+  const [itemPerLoad, setItemPerLoad] = useState(1);
 
   const canShowMore = useMemo(
-    () => displayCount < MAX_DISPLAY_COUNT,
-    [displayCount]
+    () => MIN_DISPLAY_COUNT * itemPerLoad < MAX_DISPLAY_COUNT,
+    [itemPerLoad, MAX_DISPLAY_COUNT, MIN_DISPLAY_COUNT],
   );
 
   const handleShowMore = useCallback(() => {
     if (!canShowMore) return;
-    setDisplayCount((prev) =>
-      Math.min(prev + MIN_DISPLAY_COUNT, MAX_DISPLAY_COUNT)
-    );
-  }, [displayCount, hasMore]);
+    setItemPerLoad((prev) => prev + 1);
+  }, [canShowMore]);
 
   if (posts.length === 0) {
     return <div>게시물이 없습니다.</div>;
@@ -39,12 +47,16 @@ const BlogCards = ({ posts }: BlogCardsProps) => {
   return (
     <div className="blog__cards-container">
       <div className="blog__cards">
-        {posts.slice(0, displayCount).map((post) => (
+        {posts.slice(0, MIN_DISPLAY_COUNT * itemPerLoad).map((post) => (
           <BlogCard key={post.link} {...post} />
         ))}
       </div>
       {canShowMore && (
-        <button className="blog__card-more-button" onClick={handleShowMore}>
+        <button
+          className="blog__card-more-button"
+          onClick={handleShowMore}
+          type="button"
+        >
           <LiquidGlass className="blog__card-more-button-inner">
             더 보기
           </LiquidGlass>
